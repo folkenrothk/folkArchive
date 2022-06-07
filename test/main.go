6c010bbe67eaa67fe08aed4241a9119bd0f0a8c8
Kate Folk <folkenrothk@allegheny.edu>
2022-06-07 14:47:49 -0400

    end of day: need to set up obj and parse

--- test/main.go
+++ test/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Obj struct {
+	identifier string
+	title      string
+	date       string
+	// Image
+	description    string
+	typeItem       string
+	format         string
+	PhysicalMedium string
+	Location       string
+	subject        string
+	coverage       string
+	spatial        string
+	temporal       string
+	source         string
+	creator        string
+	contributor    string
+	publisher      string
+	rights         string
+	language       string
+	// relation
+
+}
 
 func main() {
 	fmt.Println("hiya!")
+
+	var item Obj
+
 }
