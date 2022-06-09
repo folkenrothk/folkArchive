f7ec427e1feaae7b2f932271114a26c15671ca65
dluman <dluman@allegheny.edu>
2022-06-09 10:19:09 -0400

    Moving the XML further down the road

--- test/main.go
+++ test/main.go
@@ -1,36 +1,55 @@
 package main
 
 import (
+  "encoding/xml"
 	"fmt"
+  "io/ioutil"
+  "os"
 )
 
+type XML struct {
+  Obj Obj `xml:"rdf Description"`
+}
+
 type Obj struct {
-	identifier string
-	title      string
-	date       string
+  XMLName    xml.Name
+  identifier string       `xml:"dc identifier"`
+	title      string       `xml:"dc title"`
+	date       string       `xml:"dc date"`
 	// Image
-	description    string
-	typeItem       string
-	format         string
-	PhysicalMedium string
-	Location       string
-	subject        string
-	coverage       string
-	spatial        string
-	temporal       string
-	source         string
-	creator        string
-	contributor    string
-	publisher      string
-	rights         string
-	language       string
+	description    string   `xml:"dc description"`
+	typeItem       string   `xml:"dc type"`
+	format         string   `xml:"dc format"`
+	PhysicalMedium string   `xml:"dc PhysicalMedium"`
+	Location       string   `xml:"dc Location"`
+	subject        string   `xml:"dc subject"`
+	coverage       string   `xml:"dc coverage"`
+	spatial        string   `xml:"dc spatial"`
+	temporal       string   `xml:"dc temporal"`
+	source         string   `xml:"dc source"`
+	creator        string   `xml:"dc creator"`
+	contributor    string   `xml:"dc contributor"`
+	publisher      string   `xml:"dc publisher"`
+	rights         string   `xml:"dc rights"`
+	language       string   `xml:"dc language"`
 	// relation
 
 }
 
 func main() {
-	fmt.Println("hiya!")
+  xmlFile, err := os.Open("itemTrial.rdf")
 
-	var item Obj
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  defer xmlFile.Close()
+
+  fileBytes, _ := ioutil.ReadAll(xmlFile)
+
+  item := &XML{}
+  xml.Unmarshal(fileBytes, item)
+
+  fmt.Print(item.Obj)
 
 }
