83534884f43b08c280a0b50132cdb9a612b35640
Kate Folk <folkenrothk@allegheny.edu>
2022-07-05 14:45:46 -0400

    seperating success from failures

--- test/playMain.go
+++ test/playMain.go
@@ -3,39 +3,58 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"os"
 )
 
+/*
 type file struct {
 	XMLName xml.Name `xml:"RDF"`
-	rdf     struct {
-		RDFNS string `xml:"http://www.w3.org/1999/02/22-rdf-syntax-ns#"`
-		SINS  string `xml:"https://www.w3schools.com/rdf/"`
-	} `xml:"RDF"`
-	obj struct {
-		title string `xml:""https://www.w3schools.com/rdf/ title"`
-	} `xml:Description`
+	Rdf     string   `xml:"rdf,attr"`
+	dc      string   `xml:"dc,attr"`
+	Desc    string   `xml:"Description,attr"`
+}
+*/
+
+type Obj struct {
+	Creator string `xml:"creator"`
+	Title   string `xml:"title"`
+	Subject string `xml:"subject"`
 }
 
 func main() {
-	xmlFile, err := os.Open("itemTrial.rdf")
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	object_string := `
+	{
+  		"<creator> AAAAH </creator>"
+  		"<title>bruh</title>"
+  		"<subject> attempt</subject>"
+	}`
 
-	defer xmlFile.Close()
+	item1 := new(Obj)
+	xml.Unmarshal([]byte(object_string), item1)
+	fmt.Println(item1, "ugh")
+	item1Str, _ := xml.Marshal(item1)
+	fmt.Printf("%s\n", item1Str)
+	fmt.Println(item1.Creator, "is the creator")
+	/*
+		// opening the file
+		xmlFile, err := os.Open("fileTrial.rdf")
+		// if there is an err, it's handled here
+		if err != nil {
+			fmt.Println("err", err)
+		}
+		// TROUBLESHOOT-- fmt.Println("GREAT SUCCESS, the file was opened")
 
-	fileBytes, _ := ioutil.ReadAll(xmlFile)
+		// defer so we can parse it
+		defer xmlFile.Close()
 
-	var item file
+		// reading it as a byteArray
+		fileBytes, _ := ioutil.ReadAll(xmlFile)
 
-	if err := xml.Unmarshal(fileBytes, &item); err != nil {
-		panic(err)
-	}
+		var item file
 
-	fmt.Println(item)
-	fmt.Println(item.obj.title)
+		xml.Unmarshal(fileBytes, &item)
 
+		fmt.Println(item)
+		//fmt.Println(item.dc.title)
+	*/
 }
